2468a8260b76ef95739e29eb8dbe0e5f123e830b
Cèsar Encarnación <cesariux23@gmail.com>
2017-05-21 20:15:20 -0500

    Spring mvc hello word!

---
+++ src/main/java/mx/edu/ux/congresos/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package mx.edu.ux.congresos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+ 
+@Configuration
+@ComponentScan("mx.edu.ux.congresos.*") 
+public class ApplicationContextConfig { 
+  
+      
+ 
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
